8737f5854933f5a7e57fbf603cea0030c4e22907
Dean Johnson <deanjohnson222@gmail.com>
2015-04-28 14:51:50 -0700

    Fixed module imports.

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open('requirements.txt') as f:
     requirements = [l.strip() for l in f]
 
 setup(
-    name='hamper-pizza',
+    name='hamper_pizza',
     version='0.1',
-    packages=['hamper-pizza'],
+    packages=['hamper_pizza'],
     author='Dean Johnson',
     author_email='deanjohnson222@gmail.com',
     url='https://github.com/dean/hamper-pizza',
     install_requires=requirements,
-    package_data={'hamper-pizza': ['requirements.txt', 'README.md', 'LICENSE']},
+    package_data={'hamper_pizza': ['requirements.txt', 'README.md', 'LICENSE']},
     entry_points={
         'hamperbot.plugins': [
-            'pizza = hamper-pizza.pizza:Pizza',
+            'pizza = hamper_pizza.pizza:Pizza',
         ],
     },
 
